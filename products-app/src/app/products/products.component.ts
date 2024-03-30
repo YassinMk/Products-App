2236ef56fe77476d9db4c86d9cabede88f9079d3
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-30 17:16:30 +0000

    Add ProductsService and refactor ProductsComponent

--- products-app/src/app/products/products.component.ts
+++ products-app/src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -7,26 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  constructor(private http :HttpClient){
+  constructor(private ps :ProductsService ){
   }
 
   ngOnInit(): void {
-    this.http.get<Array<any>>("http://localhost:8089/products").subscribe({
-      next :data=>{this.products = data},
-      error :err=>{console.error(err)}
-  })
+    this.getProducts();
   }
-
+  
+  getProducts(){
+    this.ps.getProducts().subscribe({
+      next : products =>{
+        this.products = products;
+      },
+      error : error =>{
+        console.log(error);
+      }
+    })
+  }
 
   products : Array<any> = [];
 
   handleCheckProduct(product: any){
-    this.http.patch(`http://localhost:8089/products/${product.id}`,{checked :!product.checked})
-    .subscribe({
-      next : updatedProduct =>{
+   this.ps.checkProduct(product).subscribe({
+      next :updateProduct =>{
         product.checked = !product.checked;
+      },
+      error : error =>{
+        console.log(error);
       }
     })
-
   }
 }

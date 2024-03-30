86d1b758aba17749820da7434f87d71aac7f9bfd
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-30 17:01:12 +0000

    Update product data and add HTTP request to fetch products

--- products-app/src/app/products/products.component.ts
+++ products-app/src/app/products/products.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
-  products: Array<any> = [
-    {name:"product 1",price:100, checked:false},
-    {name:"product 2",price:200, checked:false},
-    {name:"product 3",price:300, checked:false},
-    {name:"product 4",price:400, checked:false},
-  ];
+export class ProductsComponent implements OnInit {
+  constructor(private http :HttpClient){
+  }
+
+  ngOnInit(): void {
+    this.http.get<Array<any>>("http://localhost:8089/products").subscribe({
+      next :data=>{this.products = data},
+      error :err=>{console.error(err)}
+  })
+  }
+
+
+  products : Array<any> = [];
+
   handleCheckProduct(product: any){
-    product.checked = !product.checked;
+    this.http.patch(`http://localhost:8089/products/${product.id}`,{checked :!product.checked})
+    .subscribe({
+      next : updatedProduct =>{
+        product.checked = !product.checked;
+      }
+    })
+
   }
 }

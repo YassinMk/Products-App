693ccb974f44f9975335f32b029516097b4cb0cd
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-30 18:44:33 +0000

    Add new product form to NewProductComponent

--- products-app/src/app/new-product/new-product.component.ts
+++ products-app/src/app/new-product/new-product.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductsService } from '../services/products.service';
+import { Product } from 'model/product.model';
 
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
-  styleUrls: ['./new-product.component.css']
+  styleUrls: ['./new-product.component.css'],
 })
-export class NewProductComponent {
-
+export class NewProductComponent implements OnInit {
+  public productForm!: FormGroup;
+  constructor(private fb: FormBuilder, private ps: ProductsService) {}
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      name: this.fb.control('', [Validators.required]),
+      price: this.fb.control(0),
+      checked: this.fb.control(false),
+    });
+  }
+  saveProduct() {
+    let product: Product = this.productForm.value;
+    this.ps.saveProduct(product).subscribe({
+      next: (data) => {
+        alert(JSON.stringify(data));
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }

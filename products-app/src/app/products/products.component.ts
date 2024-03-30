3296b37e02ab915bcc79650e41a9eb960458821c
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-30 19:37:46 +0000

    Refactor products component to include search functionality

--- products-app/src/app/products/products.component.ts
+++ products-app/src/app/products/products.component.ts
@@ -2,52 +2,63 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Product } from 'model/product.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  products : Array<Product> = [];
-  constructor(private ps :ProductsService ){
-  }
+  public products: Array<Product> = [];
+  public keyword: string = '';
+  constructor(private ps: ProductsService) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
-  
-  getProducts(){
+
+  getProducts() {
     this.ps.getProducts().subscribe({
-      next : products =>{
+      next: (products) => {
         this.products = products;
       },
-      error : error =>{
+      error: (error) => {
         console.log(error);
-      }
-    })
+      },
+    });
   }
 
-
-  handleCheckProduct(product: Product){
-   this.ps.checkProduct(product).subscribe({
-      next :updateProduct =>{
+  handleCheckProduct(product: Product) {
+    this.ps.checkProduct(product).subscribe({
+      next: (updateProduct) => {
         product.checked = !product.checked;
       },
-      error : error =>{
+      error: (error) => {
         console.log(error);
-      }
-    })
-  }
-  handleDeleteProduct(product :Product){
-    if(confirm("Are you sure you want to delete this product?"))
-    this.ps.deleteProduct(product).subscribe({
-      next:value=>{
-        this.products=this.products.filter((p)=>p.id !== product.id);
       },
-      error:error=>{
+    });
+  }
+  handleDeleteProduct(product: Product) {
+    if (confirm('Are you sure you want to delete this product?'))
+      this.ps.deleteProduct(product).subscribe({
+        next: (value) => {
+          this.products = this.products.filter((p) => p.id !== product.id);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+
+  searchProduct() {
+    this.ps.searchProducts(this.keyword).subscribe({
+      next: (products) => {
+        this.products = products;
+      },
+      error: (error) => {
         console.log(error);
-      }
-  })
+      },
+    });
   }
 }

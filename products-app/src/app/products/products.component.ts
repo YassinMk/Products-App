a5cd6ab405ca1d88085c26f335bf3dcb584bac88
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-30 15:37:26 +0000

    Add product list and checkbox functionality to ProductsComponent

--- products-app/src/app/products/products.component.ts
+++ products-app/src/app/products/products.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-
+  products: Array<any> = [
+    {name:"product 1",price:100, checked:false},
+    {name:"product 2",price:200, checked:false},
+    {name:"product 3",price:300, checked:false},
+    {name:"product 4",price:400, checked:false},
+  ];
+  handleCheckProduct(product: any){
+    product.checked = !product.checked;
+  }
 }

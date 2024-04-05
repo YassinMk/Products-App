0eb0e671d19efe5d3a96496dbd098757c1edf98a
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-04-05 21:28:51 +0000

    Update loading service to handle loading state in AppHttpInterceptor

--- products-app/src/app/services/app-http.interceptor.ts
+++ products-app/src/app/services/app-http.interceptor.ts
@@ -7,24 +7,26 @@ import {
 } from '@angular/common/http';
 import { Observable, finalize } from 'rxjs';
 import { AppStateService } from './app-state.service';
+import { LoadingService } from './loading.service';
 
 @Injectable()
 export class AppHttpInterceptor implements HttpInterceptor {
 
-  constructor(private appState : AppStateService) {}
-
+  constructor(private appState : AppStateService, private loadingS : LoadingService) {}
+  
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    this.appState.setProductsState({
-      status : "LOADING",
-    })
+    this.loadingS.showLoading();
+
     let req = request.clone({
       headers : request.headers.set('Authorization','Bearer JWT')
     });
     return next.handle(req).pipe(
       finalize(()=>{
-        this.appState.setProductsState({
-          status : "LOADED",
-        })
+        //this.appState.setProductsState({
+         // status : "LOADED",
+
+         this.loadingS.hideLoading();
+        
       })
     );
   }

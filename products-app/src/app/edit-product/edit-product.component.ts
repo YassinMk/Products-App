64a7c1d3b9e8507dd81e758cd949678f1cf82b8f
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-04-05 00:10:54 +0000

    Update edit-product component HTML and TypeScript files

--- products-app/src/app/edit-product/edit-product.component.ts
+++ products-app/src/app/edit-product/edit-product.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../services/products.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from 'model/product.model';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.component.html',
-  styleUrls: ['./edit-product.component.css']
+  styleUrls: ['./edit-product.component.css'],
 })
-export class EditProductComponent {
+export class EditProductComponent implements OnInit {
+  public productId!: number;
+  productFormGroup!: FormGroup;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private ps: ProductsService,
+    private fm: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.productId = this.activatedRoute.snapshot.params['id'];
+    this.ps.getProductById(this.productId).subscribe({
+      next: (product) => {
+        this.productFormGroup = this.fm.group({
+          id: this.fm.control(product.id),
+          name: this.fm.control(product.name, [Validators.required]),
+          price: this.fm.control(product.price),
+          checked: this.fm.control(product.checked),
+        });
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+  updateProduct() {
+    let product: Product = this.productFormGroup.value;
+    this.ps.updateProduct(product).subscribe({
+      next: (data) => {
+        alert(JSON.stringify(data));
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }

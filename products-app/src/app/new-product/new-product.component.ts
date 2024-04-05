de4e1cb6f1fe5b9c9e840c8c7c685d12a17ff529
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-04-05 17:30:36 +0000

    Update new-product component to handle loading and error states

--- products-app/src/app/new-product/new-product.component.ts
+++ products-app/src/app/new-product/new-product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductsService } from '../services/products.service';
 import { Product } from 'model/product.model';
+import { AppStateService } from '../services/app-state.service';
 
 @Component({
   selector: 'app-new-product',
@@ -10,11 +11,12 @@ import { Product } from 'model/product.model';
 })
 export class NewProductComponent implements OnInit {
   public productForm!: FormGroup;
-  constructor(private fb: FormBuilder, private ps: ProductsService) {}
-  
-  
-  
-  
+  constructor(
+    private fb: FormBuilder,
+    private ps: ProductsService,
+    public appState: AppStateService
+  ) {}
+
   ngOnInit(): void {
     this.productForm = this.fb.group({
       name: this.fb.control('', [Validators.required]),
@@ -23,13 +25,19 @@ export class NewProductComponent implements OnInit {
     });
   }
   saveProduct() {
+    this.appState.setProductsState({ status: 'LOADING' });
     let product: Product = this.productForm.value;
     this.ps.saveProduct(product).subscribe({
       next: (data) => {
         alert(JSON.stringify(data));
+        this.appState.setProductsState({ status: 'LOADED' });
       },
       error: (error) => {
         console.log(error);
+        this.appState.setProductsState({
+          status: 'ERROR',
+          errorMessage: error,
+        });
       },
     });
   }

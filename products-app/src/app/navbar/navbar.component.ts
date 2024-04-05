b866bda057d976096a4a4a86985758f6ffa7f3fd
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-04-05 21:29:45 +0000

    Update navbar component to use LoadingService for isLoading state

--- products-app/src/app/navbar/navbar.component.ts
+++ products-app/src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppStateService } from '../services/app-state.service';
+import { LoadingService } from '../services/loading.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,15 +8,21 @@ import { AppStateService } from '../services/app-state.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  constructor(public  appState: AppStateService) {}
   actions : Array<any> = [
     {title:"Home",route:"home" ,icon:"house"},
     {title:"Products",route:"products", icon:"search"},
     {title:"New Product",route:"newProduct", icon:"bag-plus"}
   ];
-
+  public isLoading : boolean = false;
   currentActions: any;
+
+  constructor(public  appState: AppStateService , public ls: LoadingService) { }
   setCurrentAction(action: any){
+    
     this.currentActions = action;
+    
   }
-}
+  }
+  
+  
+

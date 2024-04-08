7e3346eb80cb2b561caa99d22204055dea692dbb
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-04-08 04:20:36 +0000

    Update navbar component to include login and logout functionality

--- products-app/src/app/navbar/navbar.component.ts
+++ products-app/src/app/navbar/navbar.component.ts
@@ -1,26 +1,31 @@
 import { Component } from '@angular/core';
 import { AppStateService } from '../services/app-state.service';
 import { LoadingService } from '../services/loading.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-  actions : Array<any> = [
-    {title:"Home",route:"home" ,icon:"house"},
-    {title:"Products",route:"/admin/products", icon:"search"},
-    {title:"New Product",route:"/admin/newProduct", icon:"bag-plus"}
+  actions: Array<any> = [
+    { title: 'Home', route: 'home', icon: 'house' },
+    { title: 'Products', route: '/admin/products', icon: 'search' },
+    { title: 'New Product', route: '/admin/newProduct', icon: 'bag-plus' },
   ];
   //public isLoading : boolean = false;
   currentActions: any;
 
-  constructor(public  appState: AppStateService , public ls: LoadingService) { }
-  setCurrentAction(action: any){
+  constructor(public appState: AppStateService, public ls: LoadingService, private router:Router) {}
+  setCurrentAction(action: any) {
     this.currentActions = action;
   }
+  Login() {
+    this.router.navigateByUrl("/login");
   }
-  
-  
-
+  logout() {
+    this.appState.authState={};
+    this.router.navigateByUrl("/login");
+  }
+}
